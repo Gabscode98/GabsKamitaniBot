05fdaf94014f1aa3da10c54569c9d2d8ec0b2fc9
Gabscode98 <gabrielvg98@outlook.com>
2025-10-15 11:04:36 -0600

    mensaje del cambio

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+
 const PORT = process.env.PORT || 3000;
+const axios = require('axios');
+
+setInterval(() => {
+    axios.get(`http://localhost:${PORT}`)
+    .then(() => console.log('Ping interno exitoso'))
+    .catch(err => console.error('Error en ping interno:', err));
+}, 4 * 60 * 1000); //Cada 4 minutos
 
 app.get('/', (req, res) => {
     res.send('Bot de Discord activo!');
@@ -8,4 +16,11 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en puerto ${PORT}`);
+});
+
+process.on('unhandledRejection', (err) =>{
+    console.error('Unhandled Rejection:', err);
+});
+process.on('uncaughtException', (err) =>{
+    console.error('Unhandled Exception:', err);
 });
