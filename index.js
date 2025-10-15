ff5ffb79504a15459633afc78e35e95d5e440f4a
Gabscode98 <gabrielvg98@outlook.com>
2025-10-14 21:26:29 -0600

    mensaje del cambio

--- index.js
+++ index.js
@@ -105,12 +105,22 @@ const rest = new REST({ version: '10'}).setToken(process.env.DISCORD_TOKEN);
 
 (async () => {
     try {
-        console.log('Registrando comandos...');
+        console.log('Registrando comandos en tu servidor (guild)...');
+        // Comandos instantáneos en tu servidor
         await rest.put(
             Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
             { body: commands },
         );
         console.log(`Comandos instantáneos cargados al servidor: ${process.env.GUILD_ID}`);
+        
+        console.log('Registrando comandos globales (puede tardar hasta 1 hora)...');
+        // Comandos globales
+        await rest.put(
+            Routes.applicationCommands(process.env.CLIENT_ID),
+            { body: commands  }
+        );
+        console.log('Comandos globales registrados correctamente.');
+        
     } catch (error) {
         console.error('Error al registrar comandos:', error);
     }
