848773bf5d6a1db619658745a8b4a11ce88afd3e
Gabscode98 <gabrielvg98@outlook.com>
2025-10-20 19:52:34 -0600

    mensaje comandos de topmovimientos agregado

--- deploy-commands.js
+++ deploy-commands.js
@@ -12,7 +12,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 try {
   console.log('Registrando comandos en el servidor...');
   await rest.put(
-    Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+    Routes.applicationGuildCommands(process.env.CLIENT_ID),
     { body: commands }
   );
   console.log('Comandos registrados correctamente');
