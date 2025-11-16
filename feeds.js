ea31bb564930ab8791e5adb146feb17cc06cb51a
Gabscode98 <gabrielvg98@outlook.com>
2025-11-16 15:24:17 -0600

    mensaje comandos y noticias  agregados

---
+++ feeds.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    name: "Cageside Seats",
+    url: "https://www.cagesideseats.com/rss/index.xml"
+  },
+  {
+    name: "Joshi Puroresu News",
+    url: "https://joshipuroresunews.substack.com/feed"
+  },
+  {
+    name: "Mothlypu Puroresu",
+    url: "https://monthlypuroresu.com/feed/"
+  },
+  {
+    name: "SoloWrestling",
+    url: "https://solowrestling.com/rss/SWRSS.xml"
+  },
+  {
+    name: "SuperLuchas",
+    url: "https://superluchas.com/feed/"
+  }
+];
