a469e99baf6b7c9b4ac5408a37a5732fa802c6a3
Gabscode98 <gabrielvg98@outlook.com>
2025-12-08 21:17:28 -0600

    pruebas de ping

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ import "dotenv/config";
 import { Client, GatewayIntentBits } from "discord.js";
 import("./server.js");
 
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.once("ready", () => {
+  console.log(`✅ Bot listo como: ${client.user.tag}`);
+});
 
 client.on("interactionCreate", async interaction => {
+  if (!interaction.isChatInputCommand()) return;
 
+  if (interaction.commandName === "ping") {
+    await interaction.reply("🏓 Pong funcionando correctamente");
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
